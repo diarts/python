bd731555d5795550bf36e50bb930e501422b4932
Konstantin <diarts@mail.ru>
2018-09-19 01:22:21 +0300

    Решил задачу 1

--- algorithms/home_work_8/task_01.py
+++ algorithms/home_work_8/task_01.py
@@ -1,1 +1,39 @@
-# 1. Определение количества различных подстрок с использованием хеш-функции. Пусть дана строка S длиной N. Например, состоящая только из маленьких латинских букв. Требуется найти количество различных подстрок в этой строке. Для решения задачи рекомендую воспользоваться алгоритмом sha1 из модуля hashlib или встроенную функцию hash()
+# 1. Определение количества различных подстрок с использованием хеш-функции. Пусть дана строка S длиной N. Например,
+#  состоящая только из маленьких латинских букв. Требуется найти количество различных подстрок в этой строке. Для
+# решения задачи рекомендую воспользоваться алгоритмом sha1 из модуля hashlib или встроенную функцию hash()
+
+import hashlib
+import collections
+
+
+def hash_count(your_word):
+    '''
+    Функция принимает любую строку
+    :param your_word: принимаемая строка
+    :return: возвращает OrderedDict с ключами - подстроками и значениями - хешами этих подстрок
+    '''
+    word = str(your_word)
+
+    word_hash = hashlib.sha1(bytes(word, 'utf-8')).hexdigest()
+
+    hash_list = {}
+    collections.OrderedDict(hash_list)
+
+    for i in range(len(word)):
+        letter = ''
+        for j in word[i:]:
+            letter += j
+            hash_letter = hashlib.sha1(bytes(letter, 'utf-8')).hexdigest()
+
+            if hash_letter != word_hash and hash_letter not in hash_list:
+                hash_list[letter] = hash_letter
+
+    return hash_list
+
+
+my_word = input('Введите строку, количество хешированных подстрок которой вы хотите найти: ')
+my_word_hash = hash_count(my_word)
+print(f'\nВ вашей строке {my_word} содержится {len(my_word_hash)} хешированных подстрок:')
+
+for key, value in my_word_hash.items():
+    print('\t', f'{key} - {value}')
